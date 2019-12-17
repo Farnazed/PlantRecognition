6edad185e59de82dc09afc833152a2111a8347a2
farnazed <farnaz.e.daneshvar@gmail.com>
2019-12-16 19:24:13 -0700

    model implementned in project

--- PlantRecognition/Model/PlantClassifier.swift
+++ PlantRecognition/Model/PlantClassifier.swift
@@ -6,42 +6,12 @@
 //  Copyright © 2019 farnaz. All rights reserved.
 //
 
-import Foundation
-import Vision
-import AVFoundation
+import UIKit
+import CoreML
+
 class PlantClassifier{
-    
-  private func createClassificationRequest(modelURL: URL) -> VNCoreMLRequest? {
-    
-    fileprivate func showProductInfo(_ identifier: String) {
-        // Perform all UI updates on the main queue.
-        DispatchQueue.main.async(execute: {
-            if self.productViewOpen {
-                // Bail out early if another observation already opened the product display.
-                return
-            }
-            self.productViewOpen = true
-            self.performSegue(withIdentifier: "showProductSegue", sender: identifier)
-        })
-    }
-    
-      
-      do {
-          let objectClassifier = try VNCoreMLModel(for: MLModel(contentsOf: modelURL))
-          let classificationRequest = VNCoreMLRequest(model: objectClassifier, completionHandler: { (request, error) in
-              if let results = request.results as? [VNClassificationObservation] {
-                  print("\(results.first!.identifier) : \(results.first!.confidence)")
-                  if results.first!.confidence > 0.9 {
-                      self.showProductInfo(results.first!.identifier)
-                  }
-              }
-          })
-          return classificationRequest
-          
-      } catch let error as NSError {
-          print("Model failed to load: \(error).")
-          return nil
-      }
-  }
+ 
+  var model = FlowerShop()
   
+    
 }
